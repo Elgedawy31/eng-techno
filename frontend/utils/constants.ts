39a698dcf956205c43408b660e733137612cc281
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-11-15 01:42:04 +0200

    Update BASE_URL to use HTTPS for improved security in production environment.

--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://eng-techno-production-7ebe.up.railway.app";
+export const BASE_URL = "https://eng-techno-production-7ebe.up.railway.app";
 export const API_URL = `${BASE_URL}/api`;
