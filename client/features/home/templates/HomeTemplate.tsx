2f18adb60cbe53c9e55897d2cf396aa7b481c62b
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-11-14 14:45:45 +0200

    Remove unused banner and car components, hooks, schemas, and services to streamline the codebase.

--- client/features/home/templates/HomeTemplate.tsx
+++ client/features/home/templates/HomeTemplate.tsx
@@ -1,21 +1,9 @@
-import { AboutSection } from "../components/about/AboutSection";
-import { CarSectionsSection } from "../components/CarSectionsSection";
-import { ContactSection } from "../components/ContactSection";
-import { HeroSection } from "../components/HeroSection";
-import { WhyChooseUsSection } from "../components/whyus/WhyChooseUsSection";
-import { GoalsSection } from "@/features/home/components/goals/GoalsSection";
-import { PaymentSection } from "../components/payment/PaymentSection";
+
 
 function HomeTemplate() {
   return (
     <main className="flex flex-col gap-12">
-      <HeroSection />
-      <AboutSection />
-      <CarSectionsSection />
-      <WhyChooseUsSection />
-      <ContactSection />
-      <GoalsSection />
-      <PaymentSection />
+      home template
     </main>
   );
 }
